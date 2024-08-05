1920d5d2fa9a897a9a7cf2eafb22703951d8e859
michaeldd0120 <48832035+michaeldd0120@users.noreply.github.com>
2024-08-05 16:22:26 -0400

    Update train_torch.py

--- cellbox/cellbox/train_torch.py
+++ cellbox/cellbox/train_torch.py
@@ -140,7 +140,7 @@ def train_substage(model, lr_val, l1_lambda, l2_lambda, n_epoch, n_iter, n_iter_
                         else: return torch.matmul(model.params['W'], x)
                     w_sum = weighted_sum(x, mask)
                     matm = model.params['W']*mask
-                    raise ValueError(f"{matm}")
+                    raise ValueError(f"{model.params['W']}")
                     _dXdt = lambda x, t_mu, mask=None: model.params['eps'] * args.envelope_fn(weighted_sum(x, mask) + t_mu) - model.params['alpha'] * x
                     dxdt_current = _dXdt(x, t_mu, mask)
                     raise ValueError(f"{dxdt_current}")
