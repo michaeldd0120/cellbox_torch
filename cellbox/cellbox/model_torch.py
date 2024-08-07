351a190b1a9666ea6c95873a8a0cbc1871671447
michaeldd0120 <48832035+michaeldd0120@users.noreply.github.com>
2024-08-07 13:29:39 -0400

    Update model_torch.py

--- cellbox/cellbox/model_torch.py
+++ cellbox/cellbox/model_torch.py
@@ -119,7 +119,7 @@ class CellBox(PertBio):
         mu.requires_grad_(True)
         mu_t = torch.transpose(mu, 0, 1).requires_grad_(True)
         mask = self._get_mask().requires_grad_(True)
-        yz = self.ode_solver(y0, mu_t, self.args.dT, self.args.n_T, self._dxdt, self.args.n_activity_nodes, mask=mask).requires_grad_(True)
+        yz = self.ode_solver(y0, mu_t, self.args.dT, self.args.n_T, self._dxdt, self.gradient_zero_from, mask=mask).requires_grad_(True)
         # [n_T, n_x, batch_size]
         ys = yz[-self.args.ode_last_steps:].requires_grad_(True)
         # [n_iter_tail, n_x, batch_size]
